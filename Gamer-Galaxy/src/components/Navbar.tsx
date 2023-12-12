b3c322da437150796084c88c2f31bd07f5c43b75
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-12 21:16:35 +0530

    Build the color mode switch

--- Gamer-Galaxy/src/components/Navbar.tsx
+++ Gamer-Galaxy/src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import logo2 from "../assets/logo2.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logo} boxSize={"50px"} boxShadow="xl" rounded="md" m="2" />
-      <Text>Gamer Galaxy</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
