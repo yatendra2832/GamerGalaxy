8e12dbf4846754fc08bb0104d158a5a14aadeb86
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-13 15:51:31 +0530

    Fetching Genres

--- Gamer-Galaxy/src/App.tsx
+++ Gamer-Galaxy/src/App.tsx
@@ -2,6 +2,7 @@ import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid
@@ -14,7 +15,9 @@ function App() {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
       </Show>
 
       <GridItem area="main">
