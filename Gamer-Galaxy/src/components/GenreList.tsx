8953d009848ee4b17ce7267050c29d0e63c39c86
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-13 21:41:49 +0530

    Show a spinner while fetching the genres

--- Gamer-Galaxy/src/components/GenreList.tsx
+++ Gamer-Galaxy/src/components/GenreList.tsx
@@ -1,9 +1,11 @@
-import { List, ListItem, HStack, Image, Text } from "@chakra-ui/react";
+import { List, ListItem, HStack, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List marginY={"10px"}>
       {data.map((genre) => (
