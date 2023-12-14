19a01c1438f3a9b29047cba7f3400a2c2c8ce42f
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-14 17:05:02 +0530

    Implement Searching

--- Gamer-Galaxy/src/App.tsx
+++ Gamer-Galaxy/src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText:string
 }
 
 function App() {
@@ -30,7 +31,7 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <Navbar />
+        <Navbar onSearch={(searchText)=>setGameQuery({...gameQuery,searchText})}/>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
--- Gamer-Galaxy/src/components/Navbar.tsx
+++ Gamer-Galaxy/src/components/Navbar.tsx
@@ -3,11 +3,16 @@ import logo from "../assets/logo.webp";
 import logo2 from "../assets/logo2.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-const Navbar = () => {
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack padding={"10px"}>
       <Image src={logo} boxSize={"50px"} boxShadow="xl" rounded="md" m="2" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
