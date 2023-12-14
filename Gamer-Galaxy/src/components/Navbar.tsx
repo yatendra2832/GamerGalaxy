1e78e43bb1faf736cf3611c588946de5a27e3bc2
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-14 15:56:43 +0530

    Build Search Input

--- Gamer-Galaxy/src/components/Navbar.tsx
+++ Gamer-Galaxy/src/components/Navbar.tsx
@@ -2,10 +2,12 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import logo2 from "../assets/logo2.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const Navbar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={"10px"}>
+    <HStack padding={"10px"}>
       <Image src={logo} boxSize={"50px"} boxShadow="xl" rounded="md" m="2" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
