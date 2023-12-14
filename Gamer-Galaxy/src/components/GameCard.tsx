4cb846498cae2f9d37d6ab457b4e75adde25e2d4
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-14 17:35:28 +0530

    Clean up the game cards and set them properly

--- Gamer-Galaxy/src/components/GameCard.tsx
+++ Gamer-Galaxy/src/components/GameCard.tsx
@@ -15,8 +15,7 @@ const GameCard = ({ game }: Props) => {
       <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <HStack justifyContent={"space-between"}>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
             <PlatformIconList
               platforms={game.parent_platforms.map(
                 (p: { platform: any }) => p.platform
@@ -24,6 +23,7 @@ const GameCard = ({ game }: Props) => {
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
         </CardBody>
       </Card>
     </>
--- Gamer-Galaxy/src/components/GameGrid.tsx
+++ Gamer-Galaxy/src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ import { GameQuery } from "../App";
 interface Props {
   gameQuery: GameQuery;
 }
-const GameGrid = ({ gameQuery}: Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeltons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery}: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={"10px"}
-        spacing={4}
+        spacing={6}
       >
         {isLoading &&
           skeltons.map((skeleton) => (
