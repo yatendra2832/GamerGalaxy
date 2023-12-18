dfa8de6dec754612c0a5dfe699dbdd27d70569c5
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-18 21:48:26 +0530

    Fetching Genres

--- Gamer-Galaxy/src/main.tsx
+++ Gamer-Galaxy/src/main.tsx
@@ -5,15 +5,22 @@ import {
   ColorModeContext,
   ColorModeScript,
 } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
