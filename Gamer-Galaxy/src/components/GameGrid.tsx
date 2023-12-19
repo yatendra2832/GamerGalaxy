225f2225e78860412f103deb2febcd874c4a3921
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-19 13:59:58 +0530

    Refactoring- fetch games with the React Query

--- Gamer-Galaxy/src/components/GameGrid.tsx
+++ Gamer-Galaxy/src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const skeltons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
-      {error && <Text> {error} </Text>}
+      {error && <Text> {error.message} </Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={"10px"}
@@ -25,7 +25,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             </GameCardContainer>
           ))}
 
-        {data.map((game) => (
+        {data?.results.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
--- Gamer-Galaxy/src/hooks/usePlatforms.ts
+++ Gamer-Galaxy/src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platform";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 export interface Platform {
   id: number;
   name: string;
