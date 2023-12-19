a8da23384affaf63c816a591facad4498e38ca86
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-19 16:05:00 +0530

    update platforms

--- Gamer-Galaxy/src/hooks/useGames.ts
+++ Gamer-Galaxy/src/hooks/useGames.ts
@@ -9,9 +9,9 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
-  parent_platforms: { platform: Platform };
 }
 
 const useGames = (gameQuery: GameQuery) =>
